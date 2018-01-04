47b6dcfd50d8dad74e5095b8241bd690c57f1928
光弘 <guanghong.wsj@alibaba-inc.com>
2018-01-04 21:33:21 +0800

    test: coverage to 100%

---
+++ tests/Button.spec.jsx
@@ -0,0 +1,17 @@
+import expect from 'expect.js';
+import React from 'react';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
+import Button from '../src/Button';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Button', () => {
+  it('should render correctly', () => {
+    mount(<Button />);
+  });
+  it('props loading', () => {
+    const wrapper = mount(<Button loading />);
+    expect(wrapper.find('.kuma-button-loading-icon').length).not.to.be(0);
+  });
+});
